61d01a02c8b2c85fed76e2c716cd3ff4769980f9
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-04-06 00:02:56 +0530

    add Joker page

---
+++ src/Component/Blank.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Blank = () => {
+  return (
+    <div>Blank
+      <Link to="/home">Home</Link>
+    </div>
+  )
+}
