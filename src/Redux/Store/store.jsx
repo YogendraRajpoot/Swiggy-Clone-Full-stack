4c3e626041a9e48835899f6f0fe6ac4e24e63b16
chandu806 <76252021+chandu806@users.noreply.github.com>
2022-11-15 17:13:26 +0530

    store

--- src/Redux/Store/store.jsx
+++ src/Redux/Store/store.jsx
@@ -1,12 +1,12 @@
-import { createStore } from "redux";
-import { reducer } from "../Reducer/reducer";
-export const store = createStore(reducer, {
-  count: 0,
-  // itemss: [],
-  itemss: "",
-  amount: 0,
-  hotelname: "",
-  // location: "",
-  // isauth:"",
-  // detail:[],
-});
+// import { createStore } from "redux";
+// import { reducer } from "../Reducer/reducer";
+// export const store = createStore(reducer, {
+//   count: 0,
+//   // itemss: [],
+//   itemss: "",
+//   amount: 0,
+//   hotelname: "",
+//   // location: "",
+//   // isauth:"",
+//   // detail:[],
+// });
